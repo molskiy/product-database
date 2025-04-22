000a677c2099c9766c6174e9fa9f70ef88863ba3
molskiy <152178933+molskiy@users.noreply.github.com>
2025-04-22 21:25:41 +0300

    Update func.cpp

--- func.cpp
+++ func.cpp
@@ -1,134 +1,144 @@
 #include "func.h"
+#include <iostream>
 #include <fstream>
 using namespace std;
-
 namespace Shop {
-    Product::Product() : name(""), price(0.0), quantity(0) {}
-    
-    Product::Product(string n, double p, int q) 
-        : name(n), price(p), quantity(q) {}
-    
-    string Product::getName() const { return name; }
-    double Product::getPrice() const { return price; }
-    int Product::getQuantity() const { return quantity; }
-    
-    void Product::setName(string n) { name = n; }
-    void Product::setPrice(double p) { price = p; }
-    void Product::setQuantity(int q) { quantity = q; }
-    
-    void Product::display() const {
+    Product::Product(): name(""), price(0.0), quantity(0) {}
+    Product::Product(string n, double p, int q): name(n), price(p), quantity(q) {}
+    string Product::getName() const {return name;}
+    double Product::getPrice() const {return price;}
+    int Product::getQuantity() const {return quantity;}
+    void Product::setName(string n) {name = n;}
+    void Product::setPrice(double p) {price = p;}
+    void Product::setQuantity(int q) {quantity = q;}
+    void Product::display() const{
         cout << *this;
     }
-    
-    ostream& operator<<(ostream& os, const Product& p) {
-        os << "Name: " << p.name << ", Price: " << p.price 
-           << ", Quantity: " << p.quantity;
+    ostream& operator<<(ostream& os, const Product& p){
+        os << p.name << " " << p.price << " " << p.quantity;
         return os;
     }
-    
-    istream& operator>>(istream& is, Product& p) {
+    istream& operator>>(istream& is, Product& p){
         is >> p.name >> p.price >> p.quantity;
         return is;
     }
 
-    Database::Database(const string& fname) 
-        : products(nullptr), size(0), capacity(0), filename(fname) {
+    Database::Database(const string& fname): filename(fname), size(0), capacity(2) {
+        products = new Product[capacity];
         loadFromFile();
     }
-    
-    Database::~Database() {
+
+    Database::~Database(){
         saveToFile();
         delete[] products;
     }
-    
-    void Database::resize() {
-        capacity = capacity == 0 ? 1 : capacity * 2;
-        Product* newArray = new Product[capacity];
-        for (int i = 0; i < size; i++) {
-            newArray[i] = products[i];
+
+    void Database::resize(){
+        capacity *= 2;
+        Product* newProducts = new Product[capacity];
+        for (int i = 0; i < size; ++i){
+            newProducts[i] = products[i];
         }
         delete[] products;
-        products = newArray;
+        products = newProducts;
     }
-    
-    void Database::loadFromFile() {
-        ifstream file(filename);
-        if (file.is_open()) {
+
+    void Database::loadFromFile(){
+        ifstream inFile(filename);
+        if (inFile.is_open()){
             Product p;
-            while (file >> p) {
-                if (size == capacity) resize();
-                products[size++] = p;
+            while (inFile >> p){
+                addProduct(p);
             }
-            file.close();
+            inFile.close();
+        }
+        else{
+            cerr << "can't open the file for reading." << endl;
         }
     }
-    
-    void Database::saveToFile() {
-        ofstream file(filename);
-        if (file.is_open()) {
-            for (int i = 0; i < size; i++) {
-                file << products[i] << "\n";
+
+    void Database::saveToFile(){
+        ofstream outFile(filename);
+        if (outFile.is_open()){
+            for (int i = 0; i < size; ++i){
+                outFile << products[i] << endl;
             }
-            file.close();
+            outFile.close();
+        }
+        else{
+            cerr << "can't open file for writing." << endl;
         }
     }
-    
-    void Database::displayAll() const {
-        cout << *this;
+
+    void Database::displayAll() const{
+        for (int i = 0; i < size; ++i){
+            products[i].display();
+            cout << endl;
+        }
     }
-    
-    void Database::addProduct(const Product& p) {
-        if (size == capacity) resize();
+
+    void Database::addProduct(const Product& p){
+        if (size >= capacity){
+            resize();
+        }
         products[size++] = p;
     }
-    
-    void Database::removeProduct(int index) {
-        if (index >= 0 && index < size) {
-            for (int i = index; i < size - 1; i++) {
+
+    void Database::removeProduct(int index){
+        if (index >= 0 && index < size){
+            for (int i = index; i < size - 1; ++i){
                 products[i] = products[i + 1];
             }
-            size--;
+            --size;
+        }
+        else{
+            cerr << "index out of range" << endl;
         }
     }
-    
-    void Database::editProduct(int index) {
-        if (index >= 0 && index < size) {
-            cout << "Enter new product data (name price quantity): ";
-            cin >> products[index];
+
+    void Database::editProduct(int index){
+        if (index >= 0 && index < size){
+            cout << "add new data for product:" << endl;
+            Product newProduct;
+            cin >> newProduct;
+            products[index] = newProduct;
+        }
+        else{
+            cerr << "index out of range" << endl;
         }
     }
-    
-    void Database::searchByName(const string& name) const {
-        for (int i = 0; i < size; i++) {
-            if (products[i].getName().find(name) != string::npos) {
-                cout << products[i] << endl;
+
+
+    void Database::searchByName(const string& name) const{
+        for (int i = 0; i < size; ++i){
+            if (products[i].getName() == name){
+                products[i].display();
+                cout << endl;
             }
         }
     }
-    
-    void Database::searchByPrice(double maxPrice) const {
-        for (int i = 0; i < size; i++) {
-            if (products[i].getPrice() <= maxPrice) {
-                cout << products[i] << endl;
+
+    void Database::searchByPrice(double maxPrice) const{
+        for (int i = 0; i < size; ++i){
+            if (products[i].getPrice() <= maxPrice){
+                products[i].display();
+                cout << endl;
             }
         }
     }
-    
-    ostream& operator<<(ostream& os, const Database& db) {
-        for (int i = 0; i < db.size; i++) {
-            os << i + 1 << ". " << db.products[i] << "\n";
+    ostream& operator<<(ostream& os, const Database& db){
+        for (int i = 0; i < db.size; ++i){
+            os << db.products[i] << endl;
         }
         return os;
     }
-    
-    void showMenu() {
-        cout << "1. Display all products\n"
-                  << "2. Add new product\n"
-                  << "3. Remove product\n"
-                  << "4. Edit product\n"
-                  << "5. Search by name\n"
-                  << "6. Search by price\n"
-                  << "7. Exit\n"
-                  << "Enter number: ";
+    void showMenu(){
+        cout << "1. show products" << endl;
+        cout << "2. add product" << endl;
+        cout << "3. change product" << endl;
+        cout << "4. delete product" << endl;
+        cout << "5. search by name" << endl;
+        cout << "6. search by price" << endl;
+        cout << "7. exit" << endl;
     }
 }
