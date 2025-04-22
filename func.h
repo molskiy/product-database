47d1e877efce32e98994f5e6b89a6e880613f51f
molskiy <152178933+molskiy@users.noreply.github.com>
2025-04-22 21:26:21 +0300

    Update func.h

--- func.h
+++ func.h
@@ -2,28 +2,22 @@
 #include <iostream>
 #include <fstream>
 using namespace std;
-
 namespace Shop{
     class Product{
     private:
         string name;
         double price;
         int quantity;
-
     public:
         Product();
         Product(string n, double p, int q);
-
         string getName() const;
         double getPrice() const;
         int getQuantity() const;
-
         void setName(string n);
         void setPrice(double p);
         void setQuantity(int q);
-
         void display() const;
-
         friend ostream& operator<<(ostream& os, const Product& p);
         friend istream& operator>>(istream& is, Product& p);
     };
@@ -34,26 +28,19 @@ namespace Shop{
         int size;
         int capacity;
         string filename;
-
         void resize();
-
     public:
         Database(const string& fname);
         ~Database();
-
         void loadFromFile();
         void saveToFile();
-
         void displayAll() const;
         void addProduct(const Product& p);
         void removeProduct(int index);
         void editProduct(int index);
-
         void searchByName(const string& name) const;
         void searchByPrice(double maxPrice) const;
-
         friend ostream& operator<<(ostream& os, const Database& db);
     };
-
     void showMenu();
 }
