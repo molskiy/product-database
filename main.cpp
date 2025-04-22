b400df07b8926f34ed9fdb59dab99b2ec45e3dc7
molskiy <152178933+molskiy@users.noreply.github.com>
2025-04-22 21:23:37 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main(){
     Shop::Database db("products.txt");
     int choice;
 
@@ -10,50 +10,50 @@ int main() {
         Shop::showMenu();
         cin >> choice;
 
-        switch (choice) {
-            case 1:
-                db.displayAll();
-                break;
-            case 2: {
-                Shop::Product p;
-                cout << "Enter product data (name price quantity): ";
-                cin >> p;
-                db.addProduct(p);
-                break;
-            }
-            case 3: {
-                int index;
-                cout << "Enter product number to remove: ";
-                cin >> index;
-                db.removeProduct(index - 1);
-                break;
-            }
-            case 4: {
-                int index;
-                cout << "Enter product number to edit: ";
-                cin >> index;
-                db.editProduct(index - 1);
-                break;
-            }
-            case 5: {
-                string name;
-                cout << "Enter name to search: ";
-                cin >> name;
-                db.searchByName(name);
-                break;
-            }
-            case 6: {
-                double price;
-                cout << "Enter maximum price: ";
-                cin >> price;
-                db.searchByPrice(price);
-                break;
-            }
-            case 7:
-                cout << "Exiting...\n";
-                break;
-            default:
-                cout << "Invalid choice\n";
+        switch (choice){
+        case 1:
+            db.displayAll();
+            break;
+        case 2:{
+            Shop::Product p;
+            cout << "enter product data (name, price, quantity): ";
+            cin >> p;
+            db.addProduct(p);
+            break;
+        }
+        case 3:{
+            int index;
+            cout << "enter product number to remove: ";
+            cin >> index;
+            db.removeProduct(index - 1);
+            break;
+        }
+        case 4:{
+            int index;
+            cout << "enter product number to edit: ";
+            cin >> index;
+            db.editProduct(index - 1);
+            break;
+        }
+        case 5:{
+            string name;
+            cout << "enter name to search: ";
+            cin >> name;
+            db.searchByName(name);
+            break;
+        }
+        case 6:{
+            double price;
+            cout << "enter maximum price: ";
+            cin >> price;
+            db.searchByPrice(price);
+            break;
+        }
+        case 7:
+            cout << "exiting..." << endl;
+            break;
+        default:
+            cout << "invalid choice" << endl;
         }
     } while (choice != 7);
 
